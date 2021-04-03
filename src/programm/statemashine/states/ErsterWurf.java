a53422018f161cd9be56cb1a586e7a7f0ff69526
Julius <j.gilkaboetzow@gmail.com>
2021-04-03 17:19:24 +0200

    Erste Tests für ErsterWurf funktionieren

--- src/programm/statemashine/states/ErsterWurf.java
+++ src/programm/statemashine/states/ErsterWurf.java
@@ -1,14 +1,19 @@
 package programm.statemashine.states;
 
+import programm.newSystem.AusgabeModell;
 import programm.statemashine.Kontext;
 import programm.statemashine.NeuesAusgabeModell;
+import programm.statemashine.enums.FehlerMeldungen;
+import programm.system.spieler.Spielleiter;
 
 public class ErsterWurf implements State {
 
     Kontext kontext;
+    Spielleiter spielleiter;
 
-    public ErsterWurf(Kontext kontext) {
+    public ErsterWurf(Kontext kontext, Spielleiter spielleiter) {
         this.kontext = kontext;
+        this.spielleiter = spielleiter;
     }
 
     @Override
@@ -18,16 +23,23 @@ public class ErsterWurf implements State {
 
     @Override
     public NeuesAusgabeModell bestätigen() {
-        return null;
+        return gehtNichtErstellen();
     }
 
     @Override
     public NeuesAusgabeModell übersicht() {
-        return null;
+        return gehtNichtErstellen();
     }
 
     @Override
     public NeuesAusgabeModell zurück() {
-        return null;
+        return gehtNichtErstellen();
+    }
+
+    private NeuesAusgabeModell gehtNichtErstellen(){
+        NeuesAusgabeModell modell = new NeuesAusgabeModell(spielleiter.getGeradeDran());
+        // Brett Hinzufügen
+        modell.setFehlerMeldung(FehlerMeldungen.gehtGeradeNicht);
+        return modell;
     }
 }
--- src/tests/programm/statemashine/states/ErsterWurfTest.java
+++ src/tests/programm/statemashine/states/ErsterWurfTest.java
@@ -6,41 +6,42 @@ import programm.statemashine.Kontext;
 import programm.statemashine.NeuesAusgabeModell;
 import programm.statemashine.enums.FehlerMeldungen;
 import programm.system.spieler.Spieler;
+import programm.system.spieler.Spielleiter;
 
 import static org.mockito.Mockito.mock;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class ErsterWurfTest {
 
     Kontext kontext = mock(Kontext.class);
+    Spielleiter spielleiter = mock(Spielleiter.class);
+    Spieler spieler1 = mock(Spieler.class);
 
     ErsterWurf ersterWurf;
 
     @BeforeEach
     public void init(){
-        ersterWurf = new ErsterWurf(kontext);
+        when(spielleiter.getGeradeDran()).thenReturn(spieler1);
+        ersterWurf = new ErsterWurf(kontext, spielleiter);
     }
 
     @Test
     public void übersichtIstNichtErlaubt(){
-        NeuesAusgabeModell eigentlicheAusgabe = ersterWurf.übersicht();
-
-        assertEquals(FehlerMeldungen.gehtGeradeNicht, eigentlicheAusgabe.getFehlerMeldung());
+        gehtNichtWurdeKorrektErstellt(ersterWurf.übersicht());
     }
-
     @Test
     public void bestätigenIstNichtErlaubt(){
-        NeuesAusgabeModell eigentlicheAusgabe = ersterWurf.bestätigen();
-
-        assertEquals(FehlerMeldungen.gehtGeradeNicht, eigentlicheAusgabe.getFehlerMeldung());
+        gehtNichtWurdeKorrektErstellt(ersterWurf.bestätigen());
     }
-
     @Test
     public void zurückIstNichtErlaubt(){
-        NeuesAusgabeModell eigentlicheAusgabe = ersterWurf.zurück();
-
+        gehtNichtWurdeKorrektErstellt(ersterWurf.zurück());
+    }
+    private void gehtNichtWurdeKorrektErstellt(NeuesAusgabeModell eigentlicheAusgabe){
         assertEquals(FehlerMeldungen.gehtGeradeNicht, eigentlicheAusgabe.getFehlerMeldung());
+        assertEquals(spieler1, eigentlicheAusgabe.getGeradeDran());
     }
 
     // soll bei Wurf auf freiem/ besetztem Grundstück oder Karte landen
